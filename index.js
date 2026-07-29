b65bde070c47b835ad933690cfaf48bf187df429
Bruno Russo <brunoarusso@gmail.com>
2026-07-29 14:45:31 -0300

    Conexão com o banco MySQL

--- index.js
+++ index.js
@@ -5,3 +5,13 @@ const FileStore = require('session-file-store')(session);
 const flash = require('express-flash');
 
 const app = express();
+
+const conn = require('./db/conn');
+
+
+
+conn.sync().then(() => {
+  app.listen(3000);
+}).catch((err) => {
+  console.error('Erro ao sincronizar o banco de dados:', err);
+});
